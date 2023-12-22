dda2a51567805941498515902c25ff0bcedb800a
seanrobleto <59669333+seanrobleto@users.noreply.github.com>
2023-12-21 21:10:16 -0500

    Add files via upload

---
+++ helloworld-sean-robleto.php
@@ -0,0 +1,22 @@
+<html>
+
+<head>
+    <title>Hello, World Page</title>
+</head>
+
+<body>This is sean-robleto page
+
+<?php
+$response = file_get_contents('https://api.imgflip.com/get_memes');
+$json_response = json_decode($response);
+$img_name = $json_response->data->memes[2]->name; // Extract the third image
+$img_url = $json_response->data->memes[2]->url;
+?>
+
+<div><img src="<?php echo $img_url?>" /></div>
+
+<p><?php echo $img_name?></p>
+
+</body>
+
+</html>
